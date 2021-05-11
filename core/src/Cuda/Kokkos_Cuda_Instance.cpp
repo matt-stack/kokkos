c96ac7c232c52d5493f00bb6d8cf691c785b9e84
matt-stack <mattst@udel.edu>
2021-05-11 15:35:28 +0000

    cuda async

--- core/src/Cuda/Kokkos_Cuda_Instance.cpp
+++ core/src/Cuda/Kokkos_Cuda_Instance.cpp
@@ -119,7 +119,9 @@ int cuda_kernel_arch() {
   int arch    = 0;
   int *d_arch = nullptr;
 
-  cudaMalloc((void **)&d_arch, sizeof(int));
+  //cudaMalloc((void **)&d_arch, sizeof(int));
+  cudaMallocAsync((void **)&d_arch, sizeof(int), 0);
+  cudaDeviceSynchronize();
   cudaMemcpy(d_arch, &arch, sizeof(int), cudaMemcpyDefault);
 
   query_cuda_kernel_arch<<<1, 1>>>(d_arch);
